a24581a65b8568a452ea502699002c9c248ce300
cccaaannn <can.kurt.aa@gmail.com>
2022-10-22 01:52:06 +0300

    data folder moved

---
+++ src/app/shared/data/enums/app-routes.ts
@@ -0,0 +1,10 @@
+export enum AppRoutes {
+    SEARCH_USER = "search-user",
+    MESSAGES = "messages",
+    RESEND_VERIFICATION = "auth/resend-verification",
+    VERIFY_ACCOUNT = "auth/verify-account",
+    FORGET_PASSWORD = "auth/forget-password",
+    RESET_PASSWORD = "auth/reset-password",
+    LOGIN = "auth/login",
+    SIGNUP = "auth/signup",
+}
